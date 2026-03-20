4c3d09165f7f064e9391b8d11cf012c0fbf5f71d
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2026-03-20 11:15:53 +0600

    services section

---
+++ src/dara/services.ts
@@ -0,0 +1,44 @@
+export const services = [
+  {
+    id: 1,
+    title: "Family Law",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of",
+    image: "/images/services/family-law.png",
+  },
+  {
+    id: 2,
+    title: "Personal Injury",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of",
+    image: "/images/services/personal-injury.png",
+  },
+  {
+    id: 3,
+    title: "Business Law",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of",
+    image: "/images/services/business-law.png",
+  },
+  {
+    id: 4,
+    title: "Criminal Law",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of",
+    image: "/images/services/criminal-law.png",
+  },
+  {
+    id: 5,
+    title: "Education Law",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of",
+    image: "/images/services/education-law.png",
+  },
+  {
+    id: 6,
+    title: "Real Estate Law",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of",
+    image: "/images/services/real-estate-law.png",
+  },
+];
--- src/pages/Home/Services/Services.tsx
+++ src/pages/Home/Services/Services.tsx
@@ -1,11 +1,24 @@
 import Container from "@/components/Layout/Container";
 import SectionTitle from "@/components/Shared/Ui/SectionTitle";
+import { services } from "@/dara/services";
+import { ServiceCard } from "./ServiceCard";
 
 const Services = () => {
   return (
     <Container>
       <div className="my-14 md:my-20">
         <SectionTitle subTitle="Area Of Practice" title="How Can We Help You" />
+
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 mt-12">
+          {services.map((item) => (
+            <ServiceCard
+              key={item.id}
+              title={item.title}
+              description={item.description}
+              image={item.image}
+            />
+          ))}
+        </div>
       </div>
     </Container>
   );
